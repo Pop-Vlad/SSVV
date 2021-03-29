819310b76852fd0e9ce18f9c8b45a557eb4ca0ca
Pop-Vlad <vladpop401@gmail.com>
2021-03-29 18:38:51 +0300

    added tc_1

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -20,6 +20,30 @@ import java.util.List;
 class ServiceTest {
 
     @Test
+    void tc_1_AddStudentIdValid() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        List<Student> before = new ArrayList<>();
+        service.getAllStudenti().forEach(before::add);
+
+        Student s = new Student("id1", "name1", 936, "student@gmail.com");
+        service.addStudent(s);
+        List<Student> after = new ArrayList<>();
+        service.getAllStudenti().forEach(after::add);
+        service.deleteStudent("id1");
+        Assertions.assertEquals(before.size() + 1, after.size());
+    }
+
+    @Test
     void addStudent() {
         StudentValidator studentValidator = new StudentValidator();
         TemaValidator temaValidator = new TemaValidator();
