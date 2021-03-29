b20c3b4bed8ecc38e1601747c93c76a4ce5ea79a
Pop-Vlad <vladpop401@gmail.com>
2021-03-29 16:56:14 +0300

    first commit

---
+++ src/test/java/service/ServiceTest.java
@@ -0,0 +1,63 @@
+package service;
+
+import domain.Student;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ServiceTest {
+
+    @Test
+    void addStudent() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        List<Student> before = new ArrayList<>();
+        service.getAllStudenti().forEach(before::add);
+
+        Student s = new Student("id1", "name1", 936, "student@gmail.com");
+        service.addStudent(s);
+        List<Student> after = new ArrayList<>();
+        service.getAllStudenti().forEach(after::add);
+        service.deleteStudent("id1");
+        Assertions.assertEquals(before.size() + 1, after.size());
+    }
+
+    @Test
+    void addStudentFail() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student s = new Student("id1", "name1", -1, "student@gmail.com");
+        Assertions.assertThrows(ValidationException.class, () -> {service.addStudent(s);});
+    }
+
+}
